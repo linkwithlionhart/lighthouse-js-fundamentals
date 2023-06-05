79020eeb3c8bc46bd2133ce774aeab0337b11d9f
linkwithlionhart <linkwithlionhart@gmail.com>
2023-06-05 08:10:44 -0700

    Part 2 - Distinguished between console and log

---
+++ even.js
@@ -0,0 +1,18 @@
+// Series 1
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+
+// Series 2
+const isEven2  = function (num) {
+  return num % 2 === 0;
+}
+
+console.log(isEven2(10));
+console.log(isEven2(11));
